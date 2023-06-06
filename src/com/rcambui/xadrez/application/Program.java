fc319e270a3f8bcf8c802cd11dfb221ba383b011
Ricardo Cambui <r.cambui@hotmail.com>
2023-06-06 12:14:37 -0300

    Starting to implement Board and Piece

--- src/com/rcambui/xadrez/application/Program.java
+++ src/com/rcambui/xadrez/application/Program.java
@@ -1,12 +1,12 @@
 package com.rcambui.xadrez.application;
 
+import com.rcambui.xadrez.boardgame.Board;
 import com.rcambui.xadrez.boardgame.Position;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        var pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
