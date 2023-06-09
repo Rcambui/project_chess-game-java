b6354f958877b531613a4eb439b44404e1353620
Ricardo Cambui <r.cambui@hotmail.com>
2023-06-09 10:56:49 -0300

    Printing possible moves

--- src/com/rcambui/xadrez/chess/ChessMatch.java
+++ src/com/rcambui/xadrez/chess/ChessMatch.java
@@ -28,6 +28,13 @@ public class ChessMatch {
         return mat;
     }
 
+    public boolean[][] possibleMoves (ChessPosition sourcePosition) {
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+
+        return board.piece(position).possibleMoves();
+    }
+
     public ChessPiece performChessMove(ChessPosition sourcerPosition, ChessPosition targetPosition) {
         Position source = sourcerPosition.toPosition();
         Position target = targetPosition.toPosition();
