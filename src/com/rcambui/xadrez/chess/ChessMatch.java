cacc1f106fced805a150961aa5a4b8b573b1e142
Ricardo Cambui <r.cambui@hotmail.com>
2023-06-10 12:55:45 -0300

    Bishop

--- src/com/rcambui/xadrez/chess/ChessMatch.java
+++ src/com/rcambui/xadrez/chess/ChessMatch.java
@@ -3,6 +3,7 @@ package com.rcambui.xadrez.chess;
 import com.rcambui.xadrez.boardgame.Board;
 import com.rcambui.xadrez.boardgame.Piece;
 import com.rcambui.xadrez.boardgame.Position;
+import com.rcambui.xadrez.chess.pieces.Bishop;
 import com.rcambui.xadrez.chess.pieces.King;
 import com.rcambui.xadrez.chess.pieces.Pawn;
 import com.rcambui.xadrez.chess.pieces.Rook;
@@ -203,7 +204,9 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e',1, new King(board, Color.WHITE));
+        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -216,7 +219,9 @@ public class ChessMatch {
 
 
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
